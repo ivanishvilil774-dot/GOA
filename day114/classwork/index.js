13838ad5ac07f706b86b15dc5fb4f0debad5765d
ivanishvilil774-dot <ivanishvilil774@gmail.com>
2025-10-29 19:02:42 +0400

    second commit

---
+++ day114/classwork/index.js
@@ -0,0 +1,32 @@
+//1 
+
+const header = (
+    <div>
+        <p>hi my naem is luka and this is my first website</p>
+        <ul>
+            <li>web design</li>
+            <li>coding</li>
+            <li>digital art</li>
+        </ul>
+    </div>
+);
+
+// 2 
+
+const header2 = (
+    <section>
+        <h1>my first heading</h1>
+        <p>my first paragraph</p>
+    </section>
+);
+
+// 3 
+
+const info1 = (
+    <div>
+        <h1>Interesting is easy</h1>
+        <p>first, we need to learn some basic HTML</p>
+        <h1>headings</h1>
+        <p>heading define the outline of your site. there are six levels of headings</p>
+    </div>
+);
